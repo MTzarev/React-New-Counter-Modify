33783a33ad2b8828f7eb3a12c4589f4386888aac
Miroslav Tsarev <mtzarev@gmail.com>
2024-06-28 22:43:10 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './components/Counter.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <Counter/>
+        
+    
+      </header>
+    </div>
+  );
+}
+
+export default App;
